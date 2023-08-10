d9cc9a2f977048fa6504dfdc6ceaf0d75490e656
blotkom <abdullahahmedeladawi@gmail.com>
2023-08-10 12:10:35 -0700

    fff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main to usee printf function
- * return 555+5+5 in value hhhh
+ * main - to usee printf function
+ * return : 555+5+5 in value hhhh
 */
 int main(void){
+
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+
 }
