6f72b05ccdd36c2349572d31d229086e00e2af8e
blotkom <abdullahahmedeladawi@gmail.com>
2023-08-10 12:01:22 -0700

    fff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main
- *Retun: Always 0if succes
+ * main - start of code of the program
+ * Return: Always 0 if succes
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
