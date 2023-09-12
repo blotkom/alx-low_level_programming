36912fc08f836268657f6df6c2c83fb9d829a6d3
blotkom <abdullahahmedeladawi@gmail.com>
2023-09-12 14:05:45 -0700

    fff

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_doga dog structure
+ * @d: pointer t to free
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
